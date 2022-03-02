d93423b19a25c740c8aa2d17f13de16d78a6ae42
Colby O <60577496+Colby-O@users.noreply.github.com>
2022-03-02 17:50:45 -0400

    Add files via upload

---
+++ src/P1.js
@@ -0,0 +1,13 @@
+/*
+    Author: Sebastian Duque Rivera (A00441528)
+    Author: Felipe Duque Rivera (A00446745)
+    Author: Mo ()
+    Author: Colby ()
+
+    This file will be used throughout our project for the three phases of our service learning
+    assignment for Northwood Care. This is our javascript file.
+    
+    Current Phase: 1 , Date created: March 3, 2022.
+*/
+
+function setup() {}
