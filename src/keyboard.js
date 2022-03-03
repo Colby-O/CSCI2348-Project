58e89cc88d5490028bab4abc903edd1c0dbd5308
Colby O <60577496+Colby-O@users.noreply.github.com>
2022-03-03 02:14:12 -0400

    Added comments

--- src/keyboard.js
+++ src/keyboard.js
@@ -1,8 +1,19 @@
+/*
+ * This file contains the code for the keyboard.
+ *
+ * Author(s): Colby O'Keefe (A00428974)
+ */
 
+/*
+ * Handles logic for alphanumeric characters + space + enter (\n).
+ */
 function addChar(char) {
     $("#textbox").val($("#textbox").val() + char);
 }
 
+/*
+ * Handles logic for backspace.
+ */
 function backsapce() {
     let value = $("#textbox").val();
     if(value.length) {
