09d6ed9af9b99a0a1172812e14eae5b880272a50
Sentio101 <sdr101432@gmail.com>
2022-03-05 16:14:51 -0400

    updates and start of P1.js

--- src/P1.js
+++ src/P1.js
@@ -11,3 +11,20 @@
 */
 
 function setup() {}
+
+/**
+ * This function checks to see if any of the edit toggles are checked and retrieves the keyboard
+ * if any of them are.
+ *
+ * SDR March 5, 2022
+ */
+function getKbd() {
+  let keyboard = document.getElementById("kbd");
+  let edit1 = document.getElementById("edit1");
+  let edit2 = document.getElementById("edit2");
+  let edit3 = document.getElementById("edit3");
+
+  //Checks if edit 1, 2 or 3 is checked
+  if (edit1) {
+  }
+}
