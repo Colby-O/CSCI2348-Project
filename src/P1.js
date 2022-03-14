c3847be140ac9f3272277b96079e0cacfaefe59f
unknown <okeefeca@icloud.com>
2022-03-14 02:17:22 -0300

    Finished Save + Cancel Button

--- src/P1.js
+++ src/P1.js
@@ -10,12 +10,75 @@
   Current Phase: 1 , Date created: March 3, 2022.
 */
 
+// Stores last blog ID selected
+let currentBlogID = null;
+// Stores keybaord element
+let keyboard = null;
+// Stores blog element
+let blog = null;
+// Stores the three edit swicth elements
+let edit1 = null;
+let edit2 = null;
+let edit3 = null;
+
 /*
   Sets up the keyboard and blog list to be hidden.
+
+  Author(s): Colby O'Keefe(A00428974) + Unknown
 */
 function setup() {
-  document.getElementById("kbd").style.visibility = "hidden";
-  document.getElementById("blogArea").style.visibility = "hidden";
+  // Gets page elements
+  keyboard = document.getElementById("kbd");
+  blog = document.getElementById("blogArea");
+  edit1 = document.getElementById("edit1");
+  edit2 = document.getElementById("edit2");
+  edit3 = document.getElementById("edit3");
+
+  // hides the blog + keybaord
+  blog.style.visibility = keyboard.style.visibility = "hidden";
+
+  $("#edit1").change(()=> {
+    // Check if switch is checked 
+    if(!$("#edit1").is(":checked")) return;
+
+    // Sets selected blog id
+    currentBlogID = "blog1";
+
+    // Gets saved value from local storage if its supported by the browser
+    if (Storage !== void(0)) $("#textbox").val(window.localStorage.getItem(currentBlogID));
+    else $("#textbox").val("");
+
+    // focus the blog textarea so the keybaord will update
+    $("#textbox").focus();
+  });
+
+  $("#edit2").change(()=> {
+    // Check if switch is checked
+    if(!$("#edit2").is(":checked")) return;
+
+    // Gets saved value from local storage if its supported by the browser
+    currentBlogID = "blog2";
+    if (Storage !== void(0)) $("#textbox").val(window.localStorage.getItem(currentBlogID));
+    else $("#textbox").val("");
+
+    // focus the blog textarea so the keybaord will update
+    $("#textbox").focus();
+  });
+
+  $("#edit3").change(()=> {
+    // Check if switch is checked
+    if(!$("#edit3").is(":checked")) return;
+
+    // Sets selected blog id
+    currentBlogID = "blog3";
+
+    // Gets saved value from local storage if its supported by the browser
+    if (Storage !== void(0)) $("#textbox").val(window.localStorage.getItem(currentBlogID));
+    else $("#textbox").val("");
+
+    // focus the blog textarea so the keybaord will update
+    $("#textbox").focus();
+  });
 }
 
 /**
@@ -26,12 +89,6 @@ function setup() {
  * Modified: SDR March 12, 2022
  */
 function getKbd() {
-  let keyboard = document.getElementById("kbd");
-  let blog = document.getElementById("blogArea");
-  let edit1 = document.getElementById("edit1");
-  let edit2 = document.getElementById("edit2");
-  let edit3 = document.getElementById("edit3");
-
   let isBlogVisable = edit1.checked || edit2.checked || edit3.checked;
   let allBlogsUnchecked =  !edit1.checked && !edit2.checked && !edit3.checked;
   
@@ -47,6 +104,18 @@ function getKbd() {
  * SDR March 6, 2022 + Colby O'Keefe (A00428974)
  */
 function save() {
-  if (Storage !== void(0)) localStorage.setItem("entry", document.getElementById("textbox").value);
+  if (Storage !== void(0)) localStorage.setItem(currentBlogID, document.getElementById("textbox").value);
   else console.log("Browser doesn't support Web Storage...");
+}
+
+/*
+  Cancels the current blog edit
+
+  Author(s): Colby O'Keefe(A00428974)
+*/
+function cancel() {
+  // Unchecks each of the edit switches
+  edit1.checked = edit2.checked = edit3.checked = false;
+  // Updates the visibility of the page elements
+  getKbd();
 }
