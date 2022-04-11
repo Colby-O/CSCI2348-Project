0406e3ab65d983f751995eabd7a0509534f5c80d
Sentio101 <sdr101432@gmail.com>
2022-04-11 15:38:49 -0300

    s

--- server/db.js
+++ server/db.js
@@ -1,57 +1,59 @@
 let mySQL = require("mysql2");
-let htpassed = require("./mysqldb.inc");
+let htpassed = require("./mysqldb.js");
 
-let pool = mySQL.createPool ({
-    connectionLimit: 10,
-    host: htpassed.host,
-    port: htpassed.port,
-    user: htpassed.user,
-    password: htpassed.password,
-    database: htpassed.database,
-    charset: htpassed.charset
-
+let pool = mySQL.createPool({
+  connectionLimit: 10,
+  host: htpassed.host,
+  port: htpassed.port,
+  user: htpassed.user,
+  password: htpassed.password,
+  database: htpassed.database,
+  charset: htpassed.charset,
 });
 
 async function getBlog(id) {
-    return await new Promise((res, rej) => {
-        // Place code to get blog here!
+  return await new Promise((res, rej) => {
+    let query = `SELECT * FROM Blogs WHERE blog_id = ${id}`;
+    pool.query(query, (err, result) => {
+      res(result);
     });
+    // Place code to get blog here!
+  });
 }
 
 async function getWordBank() {
-    return await new Promise((res, rej) => {
-        // Place code to get word bank here!
-    });    
+  return await new Promise((res, rej) => {
+    // Place code to get word bank here!
+  });
 }
 
 function updateBlog(id, content) {
-    // Place code to update a blogs content here!
+  // Place code to update a blogs content here!
 }
 
 function changeBlogsPublishStatus(id, status) {
-    // Place code to chnage a blog publish status here!
+  // Place code to chnage a blog publish status here!
 }
 
-
 function addWordToBank(id) {
-    // Place code to add a word to the bank here!
+  // Place code to add a word to the bank here!
 }
 
 function deleteWordFromBank(id) {
-    // Place code to delete a word from the bank here!
+  // Place code to delete a word from the bank here!
 }
 
-// Exports 
+// Exports
 module.exports = {
-	"getBlog": getBlog,
+  getBlog: getBlog,
 
-	"updateBlog": updateBlog,
+  updateBlog: updateBlog,
 
-	"changeBlogsPublishStatus": changeBlogsPublishStatus,
+  changeBlogsPublishStatus: changeBlogsPublishStatus,
 
-    "getWordBank": getWordBank,
+  getWordBank: getWordBank,
 
-    "addWordToBank": addWordToBank,
+  addWordToBank: addWordToBank,
 
-    "deleteWordFromBank": deleteWordFromBank
+  deleteWordFromBank: deleteWordFromBank,
 };
