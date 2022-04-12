da8ebe4e67d13ad54d5a6f3c495fc5ddb092fa2b
Moh-Albashiri <100636506+Moh-Albashiri@users.noreply.github.com>
2022-04-12 17:52:31 -0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -16,9 +16,7 @@ async function getBlog(id) {
     let query = `SELECT * FROM Blogs WHERE blog_id = ${id}`;
     pool.query(query, (err, result) => {
       if (err) {
-        console.log(
-          "ERROR: " + err
-        );
+        console.log("ERROR: " + err);
       } else {
         res(result);
       }
@@ -26,10 +24,11 @@ async function getBlog(id) {
   });
 }
 
+// Created: Mohammed Al-Bashiri April 12
 async function getWordBank() {
   return await new Promise((res, rej) => {
     // Place code to get word bank here!
-    let query = `SELECT * FROM WordBank WHERE word_id = ${id}`;
+    let query = `SELECT * FROM WordBank`;
     pool.query(query, (err, result) => {
       if (err) {
         console.log("ERROR: " + err);
@@ -40,22 +39,74 @@ async function getWordBank() {
   });
 }
 
+// Created: Mohammed Al-Bashiri April 12
 function updateBlog(id, content) {
   // Place code to update a blogs content here!
+  let query = `UPDATE Blogs SET blog_content = ${content} WHERE blog_id = ${id}`;
+  pool.query(query, (err, result) => {
+    if (err) {
+      console.log("ERROR: " + err);
+    } else {
+      res(result);
+    }
+  });
 }
 
-function changeBlogsPublishStatus(id, status) {
+// Created: Mohammed Al-Bashiri April 12
+function changeBlogsPublishStatus(id, published) {
   // Place code to chnage a blog publish status here!
+  let status = published ? "P" : "NP";
+  let query = `UPDATE Blogs SET blog_status = ${status} WHERE blog_id = ${id}`;
+  pool.query(query, (err, result) => {
+    if (err) {
+      console.log("ERROR: " + err);
+    }
+  });
 }
 
-function addWordToBank(word) {
+// Created: Mohammed Al-Bashiri April 12
+async function getNumberOfWords() {
+  return await new Promise((res, rej) => {
+    let query = `SELECT COUNT(*) FROM WordBank`;
+    pool.query(numberOfWords, (err, result) => {
+      if (err) {
+        console.log("ERROR: " + err);
+      } else {
+        res(result);
+      }
+    });
+  });
+}
+
+// Created: Mohammed Al-Bashiri April 12
+async function addWordToBank(word) {
   // Place code to add a word to the bank here!
+  //if(word_id<=10){ }
+  let numberOfWords = await getNumberOfWords();
+  if (numberOfWords > 10) {
+    return;
+  }
+  let query = `INSERT INTO WordBank 
+  VALUES (${numberOfWords + 1}, ${word}, DATETIME())`;
+  pool.query(query, (err, result) => {
+    if (err) {
+      // if(word_id >10)
+      console.log("ERROR: " + err);
+    }
+  });
   // Note Terry said we need a maximum number of words let say 10
   // Send user an error if there are already 10 entries in the bank
 }
 
+// Created: Mohammed Al-Bashiri April 12
 function deleteWordFromBank(id) {
   // Place code to delete a word from the bank here!
+  let query = `DELETE FROM WordBank WHERE word_id = ${id}`;
+  pool.query(query, (err, result) => {
+    if (err) {
+      console.log("ERROR: " + err);
+    }
+  });
 }
 
 // Exports
