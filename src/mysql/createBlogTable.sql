ea4df88c07d123912b46f3ab5bc4915508fbbaef
Colby O <60577496+Colby-O@users.noreply.github.com>
2022-04-12 18:47:37 -0300

    Update createBlogTable.sql

--- src/mysql/createBlogTable.sql
+++ src/mysql/createBlogTable.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS Blogs;
 CREATE TABLE Blogs
 (
   blog_id INT(4) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  blog_content VARCHAR(MAX) NOT NULL,
+  blog_content VARCHAR(8000) NOT NULL,
   blog_status VARCHAR(2) NOT NULL,
   date_time DATETIME NOT NULL
 );
